7849cbd78698494402f60ad25d94429715c96190
prashant Srivastava <prashantsrivastava5116@gmail.com>
2023-06-06 13:34:11 +0530

    1

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const Mytask=require('./routes/TaskRouter.js')
 
 const cors = require("cors");
 app.use(express.json({limit: '50mb'}));
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })) 
+app.use(cors()) 
 app.use("/api/task",Mytask)
 
 app.listen(process.env.PORT||5000, _=> console.log("backend server is running on port: "+ process.env.PORT))
